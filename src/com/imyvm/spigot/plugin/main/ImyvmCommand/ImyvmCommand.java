c4334e15336fe566405beb0828f4ab5450433d9f
huangwang1003 <huangwang1003@gmail.com>
2018-10-03 19:33:03 +0800

    Update for future

--- src/com/imyvm/spigot/plugin/main/ImyvmCommand/ImyvmCommand.java
+++ src/com/imyvm/spigot/plugin/main/ImyvmCommand/ImyvmCommand.java
@@ -1,11 +1,17 @@
 package com.imyvm.spigot.plugin.main.ImyvmCommand;
 
+import cat.nyaa.nyaacore.Message;
 import com.imyvm.spigot.plugin.main.PluginMain;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,10 +28,11 @@ public class ImyvmCommand implements CommandExecutor{
     }
 
     public boolean onCommand(CommandSender sender, Command cmdObj, String label, String[] args) {
-        if (!sender.hasPermission("ic.command.*")) {
+        if (!sender.hasPermission("ic.command.*") && !sender.hasPermission("ic.command.tp")) {
             sender.sendMessage("You don't have the permissions!");
+            return false;
         }
-        else if(args.length==0){
+        else if(args.length <= 1){
             sender.sendMessage(ChatColor.GOLD +"------ ImyvmCommand Help ------ ");
             sender.sendMessage(ChatColor.GOLD + "/" + label + " [name]" +" create  -"+
                     ChatColor.translateAlternateColorCodes('&',"&b创建指令集"));
@@ -42,8 +49,9 @@ public class ImyvmCommand implements CommandExecutor{
             sender.sendMessage(ChatColor.GOLD + "/" + label + " [name] ex/execute [player/TRUE]  -"+
                     ChatColor.translateAlternateColorCodes('&',"&b执行指令集"));
             sender.sendMessage(ChatColor.GOLD + " ------------------------ ");
+            return false;
         }
-        else if (args[1].equalsIgnoreCase("create")) {
+        else if (args[1].equalsIgnoreCase("create") && sender.hasPermission("ic.command.*")) {
             if(args.length==2){
                 String name = args[0];
                 if (plugin.cfg.imyvmConfig.ImyvmDataList.containsKey(name)) {
@@ -55,13 +63,15 @@ public class ImyvmCommand implements CommandExecutor{
                     plugin.cfg.imyvmConfig.save();
                     sender.sendMessage("指令集 ["+args[0]+"] 已创建");
                 }
+                return true;
             }else {
                 sender.sendMessage(ChatColor.GOLD + "/" + label + " [name]" +" create  -"+
                         ChatColor.translateAlternateColorCodes('&',"&b创建指令集"));
             }
+            return false;
         }
         else if (plugin.cfg.imyvmConfig.ImyvmDataList.containsKey(args[0]) && args[1].equalsIgnoreCase
-                ("add")) {
+                ("add") && sender.hasPermission("ic.command.*")) {
             if(args.length==2){
                 sender.sendMessage(ChatColor.GOLD + "/" + label + " [name] add [command]  -"+
                         ChatColor.translateAlternateColorCodes('&',"&b添加指定至指令集"));
@@ -73,23 +83,27 @@ public class ImyvmCommand implements CommandExecutor{
                 plugin.cfg.imyvmConfig.save();
                 sender.sendMessage("指令 "+ s + " 已添加至指令集 ["+args[0]+"] ");
             }
+            return true;
         }
-        else if (args[1].equalsIgnoreCase("revoke")) {
+        else if (args[1].equalsIgnoreCase("revoke") && sender.hasPermission("ic.command.*")) {
             if(args.length==2){
                 if (plugin.cfg.imyvmConfig.ImyvmDataList.containsKey(args[0])) {
                     plugin.cfg.imyvmConfig.ImyvmDataList.remove(args[0]);
                     plugin.cfg.imyvmConfig.save();
                     sender.sendMessage("指令集 ["+args[0]+"] 已删除");
+                    return true;
                 } else {
                     sender.sendMessage("指令集["+args[0]+"]不存在");
+                    return true;
                 }
             }else {
                 sender.sendMessage(ChatColor.GOLD + "/" + label + " [name] revoke  -"+
                         ChatColor.translateAlternateColorCodes('&',"&b删除指令集"));
+                return true;
             }
         }
         else if (plugin.cfg.imyvmConfig.ImyvmDataList.containsKey(args[0]) && args[1].equalsIgnoreCase
-                ("remove")) {
+                ("remove") && sender.hasPermission("ic.command.*")) {
             if (args.length==3 && isPureDigital(args[2])){
                 int index = Integer.parseInt(args[2]);
                 List<String> command = plugin.cfg.imyvmConfig.ImyvmDataList.get(args[0]).command;
@@ -101,13 +115,15 @@ public class ImyvmCommand implements CommandExecutor{
                 } else {
                     sender.sendMessage("超出索引");
                 }
+                return true;
             }else {
                 sender.sendMessage(ChatColor.GOLD + "/" + label + " [name] remove [index]  -"+
                         ChatColor.translateAlternateColorCodes('&',"&b删除指定索引指令"));
+                return true;
             }
         }
         else if (plugin.cfg.imyvmConfig.ImyvmDataList.containsKey(args[0]) && args[1].equalsIgnoreCase
-                ("set")){
+                ("set") && sender.hasPermission("ic.command.*")){
             if (args.length<=3 || !isPureDigital(args[2])){
                 sender.sendMessage(ChatColor.GOLD + "/" + label + " [name] set [index(Int)] [command]  -"+
                         ChatColor.translateAlternateColorCodes('&',"&b修改指定索引指令"));
@@ -124,8 +140,9 @@ public class ImyvmCommand implements CommandExecutor{
                     sender.sendMessage("超出索引");
                 }
             }
+            return true;
         }
-        else if (args[1].equalsIgnoreCase("view")){
+        else if (args[1].equalsIgnoreCase("view") && sender.hasPermission("ic.command.*")){
             if (args.length==2){
                 if (plugin.cfg.imyvmConfig.ImyvmDataList.containsKey(args[0])) {
                     List<String> command = plugin.cfg.imyvmConfig.ImyvmDataList.get(args[0]).command;
@@ -141,9 +158,10 @@ public class ImyvmCommand implements CommandExecutor{
                 sender.sendMessage(ChatColor.GOLD + "/" + label + " [name] view  -"+
                         ChatColor.translateAlternateColorCodes('&',"&b查看指令集"));
             }
+            return true;
         }
         else if ((args[1].equalsIgnoreCase("execute") || args[1].equalsIgnoreCase
-                ("ex"))){
+                ("ex")) && sender.hasPermission("ic.command.*")){
             if(args.length==3){
                 if (plugin.cfg.imyvmConfig.ImyvmDataList.containsKey(args[0])) {
                     List<String> command = plugin.cfg.imyvmConfig.ImyvmDataList.get(args[0]).command;
@@ -155,6 +173,7 @@ public class ImyvmCommand implements CommandExecutor{
                         }
                     }
                     sender.sendMessage("指令集 ["+args[0]+"] 已执行");
+                    return true;
                 } else {
                     sender.sendMessage("指令集 ["+args[0]+"] 不存在");
                 }
@@ -162,11 +181,32 @@ public class ImyvmCommand implements CommandExecutor{
                 sender.sendMessage(ChatColor.GOLD + "/" + label + " [name] ex/execute [player/TRUE]  -"+
                         ChatColor.translateAlternateColorCodes('&',"&b执行指令集"));
             }
+            return true;
+        }
+        else if ((args[1].equalsIgnoreCase("teleport") || args[1].equalsIgnoreCase
+                ("tp")) && sender.hasPermission("ic.command.tp")){
+            if(args.length==2){
+                if (plugin.cfg.imyvmConfig.ImyvmDataList.containsKey(args[0])) {
+                    List<String> command = plugin.cfg.imyvmConfig.ImyvmDataList.get(args[0]).command;
+                    for (String ss:command){
+                        Location location = getLiteLocationFromString(ss);
+                        Player player = (Player) sender;
+                        player.teleport(location);
+                    }
+                    return true;
+                } else {
+                    sender.sendMessage("指令集 ["+args[0]+"] 不存在");
+                }
+            }else {
+                sender.sendMessage(ChatColor.GOLD + "/" + label + " [name] tp/teleport  -"+
+                        ChatColor.translateAlternateColorCodes('&',"&b执行传送"));
+            }
+            return true;
         }
         else {
             sender.sendMessage(ChatColor.RED +"That is not a valid command!");
+            return true;
         }
-        return true;
     }
 
     private static boolean isPureDigital(String string) {
@@ -188,6 +228,21 @@ public class ImyvmCommand implements CommandExecutor{
         }
         myString.append(args[args.length - 1]);
         return myString.toString();
+    }
+
+    private static Location getLiteLocationFromString(String s) {
+        if (s == null || s.trim().equalsIgnoreCase("")) {
+            return null;
+        }
+        final String[] parts = s.split(":");
+        if (parts.length == 4) {
+            World w = Bukkit.getServer().getWorld(parts[0]);
+            double x = Double.parseDouble(parts[1]);
+            double y = Double.parseDouble(parts[2]);
+            double z = Double.parseDouble(parts[3]);
+            return new Location(w, x, y, z);
+        }
+        return null;
     }
 
 }
